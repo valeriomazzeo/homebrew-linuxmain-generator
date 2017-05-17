767e11dafc695a6d30c1384cee26cd1b2b6b5f1f
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2017-05-17 08:50:32 +0100

    brew version update

--- linuxmain-generator.rb
+++ linuxmain-generator.rb
@@ -1,9 +1,9 @@
 class LinuxmainGenerator < Formula
   desc "Shell command to keep SPM tests in sync on OSX and Linux."
   homepage "https://github.com/valeriomazzeo/linuxmain-generator"
-  url "https://github.com/valeriomazzeo/homebrew-linuxmain-generator/archive/0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "bb32793dca645bde67ee3520350d65437c12467f59177cda3e44e9e14738ee94"
+  url "https://github.com/valeriomazzeo/homebrew-linuxmain-generator/archive/0.2.2.tar.gz"
+  version "0.2.2"
+  sha256 "c26c8a714fe12a66bd72d59e71d06f0bd61b05e4ec9bc4dee11c538b94646b9f"
 
   def install
     ENV["CC"] = ""
